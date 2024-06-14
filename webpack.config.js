bb25dcd32e0d039fb813618a96d93f2aabd8916c
Artyom Pratsko <kabanproff@gmail.com>
2024-06-14 13:52:13 +0300

    add content + styles

--- webpack.config.js
+++ webpack.config.js
@@ -10,58 +10,73 @@ const isProduction = process.env.NODE_ENV == 'production';
 const stylesHandler = MiniCssExtractPlugin.loader;
 
 
-
 const config = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devServer: {
-        open: true,
-        host: 'localhost',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html',
-        }),
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    // assetModuleFilename: 'images/[hash][ext][query]'
+  },
+  devServer: {
+    open: true,
+    host: 'localhost',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
 
-        new MiniCssExtractPlugin(),
+    new MiniCssExtractPlugin(),
 
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [stylesHandler, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpe?g|webp)$/i,
+        resourceQuery: { not: [/inline/] }, // ignore images with `?inline` query
+        type: 'asset/resource',
+        include: [/image/, /pages/], // images from `assets/images` directory only, match posix and win paths
+        generator: {
+          // output filename of images
+          filename: 'image/[name][ext]',
+        },
+      },
+      {
+        test: /\.(svg)$/i,
+        resourceQuery: /inline/,
+        type: 'asset/inline',
+      },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/i,
-                loader: 'babel-loader',
-            },
-            {
-                test: /\.css$/i,
-                use: [stylesHandler,'css-loader'],
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [stylesHandler, 'css-loader', 'sass-loader'],
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
-            },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
-        ],
-    },
+  },
 };
 
 module.exports = () => {
-    if (isProduction) {
-        config.mode = 'production';
-        
-        
-    } else {
-        config.mode = 'development';
-    }
-    return config;
+  if (isProduction) {
+    config.mode = 'production';
+
+
+  } else {
+    config.mode = 'development';
+  }
+  return config;
 };
